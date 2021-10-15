f13eda41b74030c8fd6bd208f94be7a4d7c35acc
shun1121 <hikitanikun.ware80000@gmail.com>
2021-10-15 17:15:54 +0900

    first or second commit

---
+++ src/Hello.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const Hello: React.VFC = () => {
+  const [increment, setIncrement] = useState<number>(0)
+
+  const greetingText: (date: Date) => string = (date) => {
+    const hours = date.getHours() + 1
+    console.log(new Date().getHours())
+    if (5 < hours && hours < 11) {
+      console.log(hours)
+      return 'Good Morning!!'
+    } else if (11 < hours && hours < 16) {
+      console.log(hours)
+      return 'Good Afternoon!!'
+    } else {
+      console.log(hours)
+      return 'Good Evening!!'
+    };
+
+
+    
+  }
+  const countUp = (): void => {
+    setIncrement(increment + 1)
+  }
+
+  const countDown = (): void => {
+    setIncrement(increment - 1)
+  }
+  return (
+    <>
+      <h2>Lets learn React</h2>
+      <h1>{greetingText(new Date())}</h1>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+      <p>{increment}</p>
+    </>
+  )
+}
+
+export default Hello
